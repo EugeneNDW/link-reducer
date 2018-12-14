8a1c3fc30af8d4da6aaa326563e21c3b0ab638cc
EugeneNDW <Eugene.NDW@yandex.ru>
2018-12-14 11:11:47 +0300

    add classes for original and short links

---
+++ src/main/java/ndw/eugene/Model/OriginalLink.java
@@ -0,0 +1,22 @@
+package ndw.eugene.Model;
+
+public class OriginalLink {
+
+    private String original;
+
+    public OriginalLink() {
+    }
+
+    public OriginalLink(String original) {
+        this.original = original;
+    }
+
+    public String getOriginal() {
+        return original;
+    }
+
+    public void setOriginal(String originalLink) {
+        this.original = originalLink;
+    }
+
+}
---
+++ src/main/java/ndw/eugene/Model/ShortLink.java
@@ -0,0 +1,21 @@
+package ndw.eugene.Model;
+
+public class ShortLink {
+
+    private String link;
+
+    public ShortLink() {
+    }
+
+    public ShortLink(String link) {
+        this.link = link;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
