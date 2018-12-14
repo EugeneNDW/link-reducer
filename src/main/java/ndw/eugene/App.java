5172334e678ceb476c5a0f6e3f1da4fb545f2d6a
EugeneNDW <Eugene.NDW@yandex.ru>
2018-12-14 11:08:49 +0300

    add boot application starter

---
+++ src/main/java/ndw/eugene/App.java
@@ -0,0 +1,11 @@
+package ndw.eugene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
