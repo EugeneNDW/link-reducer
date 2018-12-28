c28f696d4331b457baf2a65901b52c8ab2fa9178
EugeneNDW <Eugene.NDW@yandex.ru>
2018-12-28 10:41:16 +0300

    add class for statistics

---
+++ src/main/java/ndw/eugene/Model/Statistics.java
@@ -0,0 +1,23 @@
+package ndw.eugene.model;
+
+public class Statistics {
+
+    private int counter;
+
+    public Statistics() {
+        counter = 0;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void increase(){
+        counter++;
+    }
+
+    @Override
+    public String toString() {
+        return "counter = " + counter;
+    }
+}
