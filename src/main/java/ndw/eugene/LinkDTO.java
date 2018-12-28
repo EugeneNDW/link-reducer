b3a527e8039b2f4135f3f87d8c15cd86b87fe2d8
EugeneNDW <Eugene.NDW@yandex.ru>
2018-12-28 10:42:28 +0300

    add DTO for Link

---
+++ src/main/java/ndw/eugene/LinkDTO.java
@@ -0,0 +1,39 @@
+package ndw.eugene;
+
+import ndw.eugene.model.Link;
+
+public class LinkDTO {
+    private String link;
+    private String original;
+    private int rank;
+    private int count;
+
+
+    public LinkDTO(Link source) {
+        this.link = source.getLink().getLink();
+        this.original = source.getOriginal().getOriginal();
+        this.rank = source.getRank();
+        this.count = source.getStat().getCounter();
+
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getOriginal() {
+        return original;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+}
