7e8cf9db47447a91561770651e1e9b86a3e0cd24
EugeneNDW <Eugene.NDW@yandex.ru>
2019-01-05 19:29:51 +0300

    add statistic service dependency, exception handling

--- src/main/java/ndw/eugene/controllers/LinkController.java
+++ src/main/java/ndw/eugene/controllers/LinkController.java
@@ -1,9 +1,13 @@
 package ndw.eugene.controllers;
 
 import ndw.eugene.model.OriginalLink;
+import ndw.eugene.repository.LinkNotFoundException;
 import ndw.eugene.services.LinkService;
 import ndw.eugene.model.ShortLink;
+import ndw.eugene.services.StatisticsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,22 +16,36 @@ import org.springframework.web.bind.annotation.*;
 public class LinkController {
 
     private LinkService linkService;
+    private StatisticsService statisticsService;
 
     @Autowired
-    public LinkController(LinkService linkService) {
+    public LinkController(LinkService linkService, StatisticsService statisticsService) {
+
         this.linkService = linkService;
+        this.statisticsService = statisticsService;
+
     }
 
     @PostMapping("/generate")
     @ResponseBody
     public ShortLink generateReducedLink(@RequestBody OriginalLink link){
-        System.out.println(link.getOriginal());
-        return linkService.addLinkToStore(link);
+        return linkService.generateShortLink(link);
     }
 
     @GetMapping("/{shortLink}")
-    public String redirectToOriginalLink(@PathVariable("shortLink") String s){
-        return "redirect:" + linkService.getRawLinkByShort(s).getOriginal();
+    public String redirectToOriginalLink(@PathVariable("shortLink") String shortLink){
+
+        statisticsService.countRedirect(shortLink);
+
+        return "redirect:" + linkService.getLinkForRedirect(shortLink).getOriginal();
+
     }
+
+    @ExceptionHandler(LinkNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public HttpEntity handle(){
+        return HttpEntity.EMPTY;
+    }
+
 }
 
