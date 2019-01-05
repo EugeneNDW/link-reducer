99488e7d2f87fedae0a950bf95a4e20ebba6a8bc
EugeneNDW <Eugene.NDW@yandex.ru>
2019-01-05 19:31:44 +0300

    add LinkNotFoundException

---
+++ src/main/java/ndw/eugene/repository/LinkNotFoundException.java
@@ -0,0 +1,7 @@
+package ndw.eugene.repository;
+
+public class LinkNotFoundException extends RuntimeException {
+    public LinkNotFoundException(String message) {
+        super(message);
+    }
+}
