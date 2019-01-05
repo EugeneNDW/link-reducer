6823b97c64d3bff7f09d7b0367c701511c7e32cf
EugeneNDW <Eugene.NDW@yandex.ru>
2019-01-05 19:33:56 +0300

    add borders for count

--- src/main/java/ndw/eugene/services/StatisticsService.java
+++ src/main/java/ndw/eugene/services/StatisticsService.java
@@ -1,12 +1,10 @@
 package ndw.eugene.services;
 
-import ndw.eugene.LinkDTO;
 import ndw.eugene.model.Link;
-import ndw.eugene.repository.LinkStore;
+import ndw.eugene.repository.Store;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -14,79 +12,82 @@ import java.util.List;
 @Service
 public class StatisticsService {
 
-    private LinkStore linkStore;
-
-    private Comparator<Link> comparator = (l1, l2)->{
-        if(l1.getStat().getCounter()>l2.getStat().getCounter()){
-            return -1;
-        } else if(l1.getStat().getCounter()==l2.getStat().getCounter()){
-            return 0;
-        }
-        return 1;
-    };
+    private Store linkStore;
 
     @Autowired
-    public StatisticsService(LinkStore store) {
+    public StatisticsService(Store store) {
         this.linkStore = store;
     }
 
-    public LinkDTO getStatById(String shortLink){
-        List<Link> sortedListOfLinks = getSortedListOfLinks();
-        int rank = sortedListOfLinks.indexOf(linkStore.getLink(shortLink));
+    public Link getStatById(String shortLink){
 
-        return toLinkDTO(sortedListOfLinks.get(rank), rank+1);
+        List<Link> sortedListOfLinks = getSortedListOfLinks();
+
+        Link link = linkStore.getLink(shortLink);
+
+        int rank = sortedListOfLinks.indexOf(link);
+        Link l = sortedListOfLinks.get(rank);
+        l.setRank(rank+1);
+
+        return l;
+
     }
 
-    public List<LinkDTO> getAll(int page, int count){
+    public List<Link> getAll(int page, int count){
+
         List<Link> sortedListOfLinks = getSortedListOfLinks();
         List<Link> currentPage = getPageContent(sortedListOfLinks, page, count);
 
-        return transformToListOfDTOs(currentPage);
+        return currentPage;
+
     }
 
-    private LinkDTO toLinkDTO(Link link, int rank){
-        LinkDTO linkDTO = new LinkDTO(link);
-        linkDTO.setRank(rank);
+    public void countRedirect(String shortLink){
 
-        return linkDTO;
+        Link l = linkStore.getLink(shortLink);
+        l.countRedirect();
+
     }
+
 
     private List<Link> getSortedListOfLinks(){
+
         List<Link> list = linkStore.getAllLinks();
-        list.sort(comparator);
+        list.sort(linkComparator);
 
         for(int i=0; i<list.size(); i++){
             list.get(i).setRank(i+1);
         }
 
         return list;
-    }
 
-    private List<LinkDTO> transformToListOfDTOs(List<Link> list){
-        List<LinkDTO> result = new ArrayList<>();
-        for(int i = 0; i<list.size();i++){
-            result.add(new LinkDTO(list.get(i)));
-        }
-
-        return result;
     }
 
     private List<Link> getPageContent(List<Link> list, int page, int count) {
-        int start = getStartPosition(page, count, list.size());
-        int end = getEndPosition(page, count, list.size());
+
+        int validCount = getCountInsideBorders(count);
+        int start = getStartPosition(page, validCount, list.size());
+        int end = getEndPosition(page, validCount, list.size());
 
         return list.subList(start, end);
+
     }
 
     private int getStartPosition(int page, int count, int size){
+
         int probPosition = calculateStartPosition(page, count);
+
         return probPosition > size? size : probPosition;
+
     }
 
 
     private int getEndPosition(int page, int count, int size){
+
         int probPosition = calculateEndPosition(page, count);
+
         return probPosition > size? size : probPosition;
+
     }
 
     private int calculateStartPosition(int page, int count) {
@@ -96,4 +97,26 @@ public class StatisticsService {
     private int calculateEndPosition(int page, int count){
         return page*count;
     }
+
+    private int getCountInsideBorders(int count){
+
+        if (count<1){
+            return 1;
+        }
+
+        if (count>100){
+            return 100;
+        }
+
+        return count;
+    }
+
+    private Comparator<Link> linkComparator = (l1, l2)->{
+        if(l1.getStat().getCounter()>l2.getStat().getCounter()){
+            return -1;
+        } else if(l1.getStat().getCounter()==l2.getStat().getCounter()){
+            return 0;
+        }
+        return 1;
+    };
 }
