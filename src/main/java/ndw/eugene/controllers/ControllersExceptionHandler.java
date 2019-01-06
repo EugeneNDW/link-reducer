cc5525e3e12eed7cf0354bda203b2625d469570a
EugeneNDW <Eugene.NDW@yandex.ru>
2019-01-06 19:09:53 +0300

    added Exception handling for controllers

---
+++ src/main/java/ndw/eugene/controllers/ControllersExceptionHandler.java
@@ -0,0 +1,19 @@
+package ndw.eugene.controllers;
+
+import ndw.eugene.DTO.ExceptionDTO;
+import ndw.eugene.repository.LinkNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ControllersExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(LinkNotFoundException.class)
+    public ResponseEntity<ExceptionDTO> handleLinkNotFoundException(LinkNotFoundException e){
+        return new ResponseEntity<>(new ExceptionDTO(HttpStatus.BAD_REQUEST.value(),e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+}
