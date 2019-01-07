b8821c8cbad1e2e8bdbb2d567a437c55c748836b
EugeneNDW <Eugene.NDW@yandex.ru>
2019-01-07 13:49:37 +0300

    DTO changes for new model

---
+++ src/main/java/ndw/eugene/DTO/ShortLinkDTO.java
@@ -0,0 +1,19 @@
+package ndw.eugene.DTO;
+
+import ndw.eugene.model.Link;
+
+public class ShortLinkDTO {
+    private String link;
+
+    public ShortLinkDTO(Link link) {
+        this.link = link.getLink();
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
